be0eb2c13c62db5494dde7309be86c3289add817
dongshao <helovecandy@gmail.com>
2018-09-05 22:36:18 +0800

    add debug

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k#j0znqd=m0k74gz-*!$lazg*w$b(=3vc4ky7!q^+t1i)w^w_!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.xiaoyunliu.pro']
